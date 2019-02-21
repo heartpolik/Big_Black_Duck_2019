9b4ab8dfea459edb81b42abb64919c7bb4fe4614
kpolischuk <kostiantyn.polishchuk@ria.com>
2019-02-21 20:21:53 +0200

    ss

--- calculations.js
+++ calculations.js
@@ -6,34 +6,68 @@ module.exports = {
 		for (let i = 1; i <= maxArea; i++) {
 			let j = maxArea / i;
 			if (j % 1 === 0 && i * j >= minCount ^ 2) {
-				result.push([i, j]);
+				result.push({x: i, y: j});
 			}
 		}
 	},
 
-	getBounds(X,Y){
+	wayToPick(bound, pick) {
+		let result = [];
+		for (let xb = bound.x; xb <= pick.x; xb++) {
+			for (let yb = bound.y; yb >= pick.y; yb++) {
+				result.push({x: xb, y: yb})
+			}
+		}
+		return result;
+	},
+	getBounds(X, Y) {
 		let row = pizza[Y];
-		let xi = X;
-		let yi = Y;
-		let i = "";
-		while (xi >=0 && i!=="*") {
+		let xi  = X;
+		let yi  = Y;
+		let i   = "";
+		while (xi >= 0 && i !== "*") {
 			i = row[xi];
 			xi--;
 		}
-		while(yi >=0 && i!=="*"){
-			i=pizza[yi][xi];
+		while (yi >= 0 && i !== "*") {
+			i = pizza[yi][xi];
 			yi--;
 		}
-		return {xi, yi}
+		return {x: xi, y: yi}
+	},
+	checkArea(x1, y1, x2, y2, good, bad, pizza) {
+		let sum = 0;
+		for (let x = x1; x <= x2; x++) {
+			for (let y = y1; y <= y2; y++) {
+				switch (pizza[x][y]) {
+					case good:
+						sum++;
+						break;
+					case bad:
+						sum--;
+				}
+			}
+		}
+		return sum;
 	},
 	getSlicePrices(slices, pick, pizza, good, bad) {
-		let y = pick[0],
-				x = pick[1];
+		let y      = pick[0],
+				x      = pick[1];
+		let result = [];
 
-		let bounds = getBounds(pick[0], pick[1]);
-		slices.forEach(slice=>{
-
-
-		})
+		let bounds = getBounds(x, y);
+		let wtp    = wayToPick(bounds, pick);
+		wtp.forEach(point => {
+			slices.forEach(slice => {
+				let x1 = point.x;
+				let x2 = slice.x + point.x;
+				let y1 = point.y;
+				let y2 = slice.y + point.y;
+				if (pizza[x2][y2]) {
+					result.push({x1, y1, x2, y2, price: checkArea(x1, y1, x2, y2, good, bad, pizza)})
+				}
+			})
+		});
+		return result;
 	}
 };
