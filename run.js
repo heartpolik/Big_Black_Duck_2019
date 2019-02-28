c567ae44947e070dfe94803e462fa9062f998c78
zagnit <oleh.zahnitko@ria.com>
2019-02-28 22:28:55 +0200

    parser

--- run.js
+++ run.js
@@ -5,36 +5,40 @@ const _ = require('lodash')
  * @returns {*}
  */
 module.exports = async ({items, photosCount}) => {
+  let lastPhoto;
   let lastProcessedPhotoIndex;
+  let i = 0;
   let slides = [];
   let minPhoto;
   for (let index in items) {
     minPhoto = minPhoto || index;
-    if (items[index].tagsCount < items[minPhoto].tagsCount) minPhoto = index;
+    if (items[index].tagsCount > items[minPhoto].tagsCount) minPhoto = index;
   }
 
   processSlide(items[minPhoto]);
-  runFind(slides, items);
+  runFind(slides);
 
   return slides;
 
   function pushSlide(slide1, slide2) {
     if (slide2) {
       slides.push(`${slide1.index} ${slide2.index}`) //махнути местом
+      lastPhoto = slide2;
     } else {
-      slides.push(`${slide1.index}`)
+      slides.push(`${slide1.index}`);
+      lastPhoto = slide1;
     }
   }
 
 
   function processSlide(slide) {
+
     if (slide.h) {
       pushSlide(slide);
       delete items[slide.index];
     } else {
       let slide2;
       delete items[slide.index];
-      return;
 
       slide2 = findSimilar(slide, 'v');
 
@@ -49,52 +53,60 @@ module.exports = async ({items, photosCount}) => {
           }
         }
       }
-      pushSlide(slide, slide2);
+      slide2 && pushSlide(slide, slide2);
     }
   }
 
 
-  function runFind(slides, items) {
+  function runFind(slides) {
+    i++;
     //lastProcessedPhotoIndex =
     let _keys = Object.keys(items);
     console.log("_keys : " + _keys.length);
-    if (!_keys.length) {
+    if (!_keys.length || i > 1000) {
       return;
     }
-    let nextSlide = findSimilar(_.last(slides));
+
+    let nextSlide = findSimilar(lastPhoto);
     if (!nextSlide) nextSlide = items[_keys[0]];
     if (nextSlide) {
       processSlide(nextSlide);
-      return runFind(slides, items);
+      return runFind(slides);
     }
     return;
 
   }
+
   function findSimilar(slide1, orientation) {
     for (let index in items) {
-      if (isSimilar(items[index], slide1)) {
+      if (getMin(items[index], slide1)) {
         if (!orientation || (orientation && items[index][orientation])) {
           return items[index];
         }
       }
     }
   }
+
+  // function getLast() {
+  //   let last = _.last(slides);
+  //   return items[_.last(last.split(' '))];
+  // }
 };
 
 
-
-
-
-
-function isSimilar(slide1, slide2) {
-  let args, min;
+function getMin(slide1, slide2) {
+  let args, minPhoto, maxPhoto;
   if (slide1.tagsCount >= slide2.tagsCount) {
-    args = [slide1, slide2];
-    min = slide2.tagsCount;
+    minPhoto = slide2;
+    maxPhoto = slide1;
   } else {
-    args = [slide2, slide1];
-    min = slide1.tagsCount;
+    minPhoto = slide1;
+    maxPhoto = slide2;
   }
-  let dif = _.difference(...args);
-  return dif.length <= min;
+
+  let dif1 = _.difference(minPhoto.tags, maxPhoto.tags).length;
+  let dif2 = _.difference(maxPhoto.tags, minPhoto.tags).length;
+  let dif3 = maxPhoto.tagsCount - dif2;
+  let min = Math.min(dif1, dif2, dif3);
+  return min;
 }
